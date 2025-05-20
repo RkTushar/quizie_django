a11c2a09f4865d6d31d4f95a49392f8debc5dc37
Rezaul Karim Tushar <53859459+RkTushar@users.noreply.github.com>
2025-05-21 04:10:33 +0600

    Update admin.py

--- quizie/quiz/admin.py
+++ quizie/quiz/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category, Quiz, Question, Choice
 
-# Register your models here.
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ('text', 'quiz')
+
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'created_at')
+
+admin.site.register(Category)
+admin.site.register(Quiz, QuizAdmin)
+admin.site.register(Question, QuestionAdmin)
