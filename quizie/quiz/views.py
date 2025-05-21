6aced9ca53e78a2ca16f85d8b29522b9f14b6c37
Rezaul Karim Tushar <53859459+RkTushar@users.noreply.github.com>
2025-05-22 01:54:16 +0600

    Update views.py

--- quizie/quiz/views.py
+++ quizie/quiz/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Quiz, Question, Choice
 
-# Create your views here.
+def quiz_list(request):
+    quizzes = Quiz.objects.all()
+    return render(request, 'quiz/quiz_list.html', {'quizzes': quizzes})
+
+def quiz_detail(request, quiz_id):
+    quiz = get_object_or_404(Quiz, pk=quiz_id)
+    questions = quiz.questions.all()
+
+    if request.method == 'POST':
+        score = 0
+        for question in questions:
+            selected = request.POST.get(str(question.id))
+            if selected:
+                choice = Choice.objects.get(pk=int(selected))
+                if choice.is_correct:
+                    score += 1
+        return render(request, 'quiz/result.html', {'score': score, 'total': questions.count()})
+
+    return render(request, 'quiz/quiz_detail.html', {'quiz': quiz, 'questions': questions})
